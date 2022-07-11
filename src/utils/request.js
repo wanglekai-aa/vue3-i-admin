2adc66e9e9eb4d7bb2b6eb51fd3bf3dfdd6e62b8
wanglekai <xiaoguailele@foxmail.com>
2022-07-12 00:37:27 +0800

    feat(axios): 配置环境变量封装 axios 模块
    环境变量 和 service 请求基础封装

---
+++ src/utils/request.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+service.interceptors.request.use((config) => {
+  config.headers.icode = 'A353F300C8E83D1F'
+  return config
+})
+
+export default service
