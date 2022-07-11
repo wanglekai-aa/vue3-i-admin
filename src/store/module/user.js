fa5fc77e1cbcfbb6e7fa0ec625c519dc84ed04bc
wanglekai <xiaoguailele@foxmail.com>
2022-07-12 01:50:32 +0800

    feat(user-token): 本地缓存处理方案
    登录完成后需要 将token 存储 vuex 和 本地storage 中

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,11 +1,19 @@
 // 处理所有和 用户相关 的内容
-
+import { TOKEN } from '@/constant'
+import { setItem, getItem } from '@/utils/storage'
 import { login } from '@/api/sys'
 import md5 from 'md5'
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(context, userInfo) {
       const { username, password } = userInfo
@@ -15,6 +23,7 @@ export default {
           password: md5(password)
         })
           .then((res) => {
+            this.commit('User/setToken', res.data.data.token)
             resolve(res.data)
           })
           .catch((err) => {
