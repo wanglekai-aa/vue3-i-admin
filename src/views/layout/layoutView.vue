e92632995b9ce5d16e52ad0345186a54e8a09b8b
wanglekai <xiaoguailele@foxmail.com>
2022-07-12 11:09:37 +0800

    feat(layout): 页面基本机构搭建
    基础 样式，公共 scss 变量，mixin

--- src/views/layout/layoutView.vue
+++ src/views/layout/layoutView.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="">Layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <!-- 主题更换，所以为 sidebar 赋值动态的背景颜色 -->
+    <com-slidebar
+      id="guide-sidebar"
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    />
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部的 navbar -->
+        <com-navbar />
+      </div>
+      <!-- 内容区 -->
+      <app-main />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import ComNavbar from '@/components/ComNavbar/ComNavbar.vue'
+import ComSlidebar from '@/components/ComSlidebar/ComSlidebar.vue'
+import AppMain from '@/components/AppMain/AppMain.vue'
+
+import variables from '@/styles/variables.module.scss'
+console.log(variables)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.module.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
