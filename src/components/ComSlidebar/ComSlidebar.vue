982c907eb88063b21df9e2eed4e9a7251bf2635b
wanglekai <xiaoguailele@foxmail.com>
2022-07-12 16:47:51 +0800

    feat(menu 菜单): 临时 侧边 menu 菜单结构

--- src/components/ComSlidebar/ComSlidebar.vue
+++ src/components/ComSlidebar/ComSlidebar.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="">Sidebar</div>
+  <div class="sidebar-container">
+    <h2>占位</h2>
+    <el-scrollbar>
+      <sidebar-menu />
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
+import SidebarMenu from './SidebarMenu.vue'
 import {} from 'vue'
 </script>
 
