5028af17008b6e66d33a3ab255fad4f1e792ff98
wanglekai <xiaoguailele@foxmail.com>
2022-07-12 19:53:04 +0800

    feat(动态菜单): 生成动态 menu 菜单

--- src/components/ComSlidebar/SidebarMenu.vue
+++ src/components/ComSlidebar/SidebarMenu.vue
@@ -7,24 +7,12 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子集 menu 菜单 -->
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-submenu>
-    <!-- 具体菜单项 -->
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
+    <sidebar-item v-for="route in routes" :key="route.path" :route="route" />
   </el-menu>
 </template>
 
 <script setup>
+import SidebarItem from './SidebarItem.vue'
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
