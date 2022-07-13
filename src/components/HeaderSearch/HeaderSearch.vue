da9ba49e78c2143f5b502009984b04d203c1c74d
wanglekai <xiaoguailele@foxmail.com>
2022-07-13 22:35:47 +0800

    fix(headerSearch): headerSearch 剩余问题处理
    在 search 打开时，点击 body 关闭 search, 在 search 关闭时，清理 searchOptions, headerSearch 国际化处理

--- src/components/HeaderSearch/HeaderSearch.vue
+++ src/components/HeaderSearch/HeaderSearch.vue
@@ -27,11 +27,12 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { generateRoutes } from './FuseData'
 import Fuse from 'fuse.js'
 import { filterRouters } from '@/utils/route'
 import { useRouter } from 'vue-router'
+import { watchSwitchLang } from '@/utils/i18n'
 
 // 控制 search 显示
 const isShow = ref(false)
@@ -64,7 +65,7 @@ const querySearch = (query) => {
 
 // 检索数据源
 const router = useRouter()
-const searchPool = computed(() => {
+let searchPool = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   // console.log(generateRoutes(filterRoutes))
   return generateRoutes(filterRoutes)
@@ -74,24 +75,57 @@ const searchPool = computed(() => {
 /**
  * 搜索库相关
  */
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
+let fuse
+const initFuse = (searchPool) => {
+  fuse = new Fuse(searchPool, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+
+initFuse(searchPool.value)
+
+/**
+ * 关闭 search 的处理事件
+ */
+const onClose = () => {
+  headerSearchSelectRef.value.blur()
+  isShow.value = false
+  searchOptions.value = []
+}
+/**
+ * 监听 search 打开，处理 close 事件
+ */
+watch(isShow, (val) => {
+  if (val) {
+    document.body.addEventListener('click', onClose)
+  } else {
+    document.body.removeEventListener('click', onClose)
+  }
+})
+
+// 处理国际化
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    const filterRoutes = filterRouters(router.getRoutes())
+    return generateRoutes(filterRoutes)
+  })
+  initFuse(searchPool.value)
 })
 </script>
 
