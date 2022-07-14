d73de113a22cfdccdb1b11f9f7c87312a9ddfd09
wanglekai <xiaoguailele@foxmail.com>
2022-07-14 16:15:06 +0800

    feat(个人中心模块): 个人中心模块基本结构

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="">
-    <h2>{{ $t('msg.route.profile') }}</h2>
-
+  <div class="my-container">
     <el-row>
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
+      <el-col :span="6">
+        <project-card class="user-card"></project-card>
+      </el-col>
+      <el-col :span="18">
+        <el-card>
+          <el-tabs v-model="activeName">
+            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
+              <project-feature />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
+              <project-chapter />
+            </el-tab-pane>
+            <el-tab-pane :label="$t('msg.profile.author')" name="author">
+              <project-author />
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import ProjectCard from './components/ProjectCard.vue'
+import ProjectChapter from './components/ProjectChapter.vue'
+import ProjectFeature from './components/ProjectFeature.vue'
+import ProjectAuthor from './components/ProjectAuthor.vue'
+import { ref } from 'vue'
+const activeName = ref('feature')
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.my-container {
+  .user-card {
+    margin-right: 20px;
+  }
+}
+</style>
---
+++ src/views/profile/components/ProjectCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <div>项目介绍</div> -->
+  <pan-thumb
+    :image="require('@/assets/logo.jpg')"
+    :height="'100px'"
+    :width="'100px'"
+    :hoverable="false"
+  >
+    <div>Hello</div>
+    {{ $store.getters.userInfo.title }}
+  </pan-thumb>
+</template>
+
+<script setup>
+import PanThumb from '@/components/PanThumb/PanThumb.vue'
+</script>
