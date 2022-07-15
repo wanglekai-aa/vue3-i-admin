a52f9d53a46484a96a1bd90fb50fe97b01167ede
wanglekai <xiaoguailele@foxmail.com>
2022-07-15 10:07:49 +0800

    feat(profile 个人中心): 完善个人中心右侧内容
    个人中心右侧内容：功能，章节和作者

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,15 @@
 import servcie from '@/utils/request'
 
+// 功能介绍
 export const feature = () => {
   return servcie({
     url: '/user/feature'
+  })
+}
+
+// 章节内容
+export const chapter = () => {
+  return servcie({
+    url: '/user/chapter'
   })
 }
--- src/views/profile/components/ProjectAuthor.vue
+++ src/views/profile/components/ProjectAuthor.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>作者</div>
+  <div class="author-container">
+    <div class="header">
+      <pan-thumb
+        :image="require('@/assets/logo.jpg')"
+        height="60px"
+        width="60px"
+        :hoverable="false"
+      >
+        {{ $t('msg.profile.name') }}
+      </pan-thumb>
+      <div class="header-desc">
+        <h3>{{ $t('msg.profile.name') }}</h3>
+        <span>{{ $t('msg.profile.job') }}</span>
+      </div>
+    </div>
+    <div class="info">
+      {{ $t('msg.profile.Introduction') }}
+    </div>
+  </div>
 </template>
 
-<script>
-export default {}
+<script setup>
+import PanThumb from '@/components/PanThumb/PanThumb.vue'
+import {} from 'vue'
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
