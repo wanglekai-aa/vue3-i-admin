b82e317f897926e5b3530d013e199d1ccb9a5653
wanglekai <xiaoguailele@foxmail.com>
2022-07-18 20:56:41 +0800

    feat(文章排名): 文章表格拖动排序

--- src/api/article.js
+++ src/api/article.js
@@ -7,3 +7,12 @@ export const getArticleList = (data) => {
     params: data
   })
 }
+
+// 修改排序
+export const articleSort = (data) => {
+  return request({
+    url: '/article/sort',
+    method: 'POST',
+    data
+  })
+}
